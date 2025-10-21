e1e3fc89e7a32a355c44dd950187f24149f7e3c1
niltinfilho <niltonfilho115@gmail.com>
2025-10-21 10:17:57 -0300

    criando os componentes para o crud

---
+++ crud-angular-material/.angular/cache/19.2.18/crud-angular-material/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-YOA2EOVX.js";
+import "./chunk-GKIE6DTT.js";
+import "./chunk-XP4P5ATS.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
