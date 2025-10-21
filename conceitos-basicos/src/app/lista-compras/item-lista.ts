0696b7dc3cc895c1f2a30f3caee8255c03f0da98
niltinfilho <niltonfilho115@gmail.com>
2025-10-21 10:01:17 -0300

    first commit

---
+++ conceitos-basicos/src/app/lista-compras/item-lista.ts
@@ -0,0 +1,5 @@
+export class ItemLista {
+  id?: number;
+  nome?: string;
+  isComprado: boolean = false;
+}
---
+++ conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemLista } from './item-lista';
+
+@Component({
+  selector: 'app-lista-compras',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './lista-compras.component.html',
+  styleUrl: './lista-compras.component.scss'
+})
+export class ListaComprasComponent {
+  item: string = "";
+  lista: ItemLista[] = [];
+
+  adicionarItemLista() {
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+
+    this.lista.push(itemLista);
+    this.item = "";
+  }
+
+  riscarItem(itemLista: ItemLista) {
+    itemLista.isComprado = !itemLista.isComprado;
+  }
+
+  limparLista() {
+    this.lista = [];
+  }
+}
