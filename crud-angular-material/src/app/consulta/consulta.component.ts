502bcc5caf6d1921e8441833c0cbebc58a5dfea3
niltinfilho <niltonfilho115@gmail.com>
2025-10-21 14:19:46 -0300

    fazendo a navegacao entre componentes passando parametros

--- crud-angular-material/src/app/consulta/consulta.component.ts
+++ crud-angular-material/src/app/consulta/consulta.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
-import { MatTableModule } from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButton } from "@angular/material/button";
-import { ClienteService } from '../cliente.service';
-import { Cliente } from '../cadastro/cliente';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { Cliente } from '../cadastro/cliente';
+import { ClienteService } from '../cliente.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-consulta',
@@ -23,7 +23,7 @@ import { CommonModule } from '@angular/common';
     MatButton,
     MatButtonModule,
     CommonModule
-],
+  ],
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.scss'
 })
@@ -31,9 +31,12 @@ import { CommonModule } from '@angular/common';
 export class ConsultaComponent implements OnInit {
   nomeBusca: string = '';
   listaClientes: Cliente[] = [];
-  colunasTable: string[] = ["id", "nome", "cpf", "dataNascimento", "email"]
+  colunasTable: string[] = ["id", "nome", "cpf", "dataNascimento", "email", "acoes"]
 
-  constructor(private service: ClienteService) {
+  constructor(
+    private service: ClienteService,
+    private router: Router
+  ) {
 
   }
 
@@ -43,5 +46,9 @@ export class ConsultaComponent implements OnInit {
 
   pesquisar() {
     this.listaClientes = this.service.pesquisarClientes(this.nomeBusca);
+  }
+
+  preparaEditar(id: string) {
+    this.router.navigate(['/cadastro'], { queryParams: { "id": id } })
   }
 }
