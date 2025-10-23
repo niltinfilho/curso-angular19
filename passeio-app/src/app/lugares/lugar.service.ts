3a42ec6d555f29a37c898bd7626e99b76eb03ab4
niltinfilho <niltonfilho115@gmail.com>
2025-10-23 11:01:25 -0300

    implementando pesquisa de lugares

--- passeio-app/src/app/lugares/lugar.service.ts
+++ passeio-app/src/app/lugares/lugar.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Lugar } from './lugar';
 import { Observable } from 'rxjs';
@@ -16,5 +16,23 @@ export class LugarService {
 
   obterTodas(): Observable<Lugar[]> {
     return this.http.get<Lugar[]>('http://localhost:3000/lugares');
+  }
+
+  filtrar(nome: string, categoria: string): Observable<Lugar[]> {
+    let parametros = new HttpParams();
+
+    if (nome) {
+      parametros = parametros.set('nome_like', nome);
+    }
+
+    if (categoria) {
+      parametros = parametros.set('categoria', categoria);
+    }
+
+    console.log(parametros)
+
+    return this.http.get<Lugar[]>('http://localhost:3000/lugares', {
+      params: parametros
+    });
   }
 }
