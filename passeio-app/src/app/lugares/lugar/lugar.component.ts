f06e15b16f2783420552d589a1c4a407a238a276
niltinfilho <niltonfilho115@gmail.com>
2025-10-23 09:52:51 -0300

    logica para enviar o cadastro de lugares para a api

--- passeio-app/src/app/lugares/lugar/lugar.component.ts
+++ passeio-app/src/app/lugares/lugar/lugar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Categoria } from '../../categorias/categoria';
 import { CategoriaService } from '../../categorias/categoria.service';
+import { LugarService } from '../lugar.service';
 
 @Component({
   selector: 'app-lugar',
@@ -13,13 +14,16 @@ export class LugarComponent implements OnInit{
   camposForm: FormGroup;
   categorias: Categoria[] = [];
 
-  constructor(private categoriaService: CategoriaService) {
+  constructor(
+    private categoriaService: CategoriaService,
+    private service: LugarService
+  ) {
     this.camposForm = new FormGroup({
       nome: new FormControl('', Validators.required),
-      categoria: new FormControl('', Validators.required),
+      categoria: new FormControl('0', Validators.required),
       localizacao: new FormControl('', Validators.required),
       urlFoto: new FormControl('', Validators.required),
-      avaliacao: new FormControl('', Validators.required),
+      avaliacao: new FormControl('0', Validators.required),
     });
   }
 
@@ -30,6 +34,12 @@ export class LugarComponent implements OnInit{
   }
 
   salvar() {
-    console.log('Salva com sucesso!', this.camposForm.value);
+   this.service.salvar(this.camposForm.value).subscribe({
+    next: (lugar) => {
+      console.log('Cadastrado com sucesso ', lugar);
+      this.camposForm.reset();
+    },
+    error: erro => console.error('Ocorreu um erro: ', erro)
+   })
   }
 }
