f9859a122d9d2a6eadf922f27dbe20651b6929ca
niltinfilho <niltonfilho115@gmail.com>
2025-10-23 08:28:32 -0300

    criando service para consumir as categorias

---
+++ passeio-app/src/app/categorias/categoria.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Categoria } from './categoria';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  constructor(private http: HttpClient) { }
+
+  salvar(categoria: Categoria): Observable<Categoria> {
+    return this.http.post<Categoria>('http://localhost:3000/categorias', categoria);
+  }
+
+  obterTodas(): Observable<Categoria[]> {
+    return this.http.get<Categoria[]>('http://localhost:3000/categorias');
+  }
+}
