c5798513874f34494c6938e38a235c4c6e856f3a
niltinfilho <niltonfilho115@gmail.com>
2025-10-23 11:53:08 -0300

    criando a página inicial da aplicacao

--- passeio-app/src/app/app.module.ts
+++ passeio-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideHttpClient, withFetch } from '@angular/common/http'
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { LandingpageComponent } from './landingpage/landingpage.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent
   ],
   imports: [
     BrowserModule,
