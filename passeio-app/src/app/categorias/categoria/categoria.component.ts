1401121571433db871d699d468c41c162f818b5c
niltinfilho <niltonfilho115@gmail.com>
2025-10-23 09:17:38 -0300

    adicionando os campos de formulario de lugar

--- passeio-app/src/app/categorias/categoria/categoria.component.ts
+++ passeio-app/src/app/categorias/categoria/categoria.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CategoriaService } from '../categoria.service';
 
 @Component({
--- passeio-app/src/app/lugares/lugar/lugar.component.ts
+++ passeio-app/src/app/lugares/lugar/lugar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Categoria } from '../../categorias/categoria';
 
 @Component({
   selector: 'app-lugar',
@@ -7,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './lugar.component.scss'
 })
 export class LugarComponent {
+  camposForm: FormGroup;
+  categorias: Categoria[] = [];
 
+  constructor() {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      categoria: new FormControl('', Validators.required),
+      localizacao: new FormControl('', Validators.required),
+      urlFoto: new FormControl('', Validators.required),
+      avaliacao: new FormControl('', Validators.required),
+    });
+  }
+
+  salvar() {
+    console.log('Salva com sucesso!', this.camposForm.value);
+  }
 }
