63407b012ecce3392a0cb6e6f96dc97d6228f236
niltinfilho <niltonfilho115@gmail.com>
2025-10-23 09:37:42 -0300

    listando categorias e avaliacao

--- passeio-app/src/app/lugares/lugar/lugar.component.ts
+++ passeio-app/src/app/lugares/lugar/lugar.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Categoria } from '../../categorias/categoria';
+import { CategoriaService } from '../../categorias/categoria.service';
 
 @Component({
   selector: 'app-lugar',
@@ -8,17 +9,23 @@ import { Categoria } from '../../categorias/categoria';
   templateUrl: './lugar.component.html',
   styleUrl: './lugar.component.scss'
 })
-export class LugarComponent {
+export class LugarComponent implements OnInit{
   camposForm: FormGroup;
   categorias: Categoria[] = [];
 
-  constructor() {
+  constructor(private categoriaService: CategoriaService) {
     this.camposForm = new FormGroup({
       nome: new FormControl('', Validators.required),
       categoria: new FormControl('', Validators.required),
       localizacao: new FormControl('', Validators.required),
       urlFoto: new FormControl('', Validators.required),
       avaliacao: new FormControl('', Validators.required),
+    });
+  }
+
+  ngOnInit(): void {
+    this.categoriaService.obterTodas().subscribe({
+      next: (listaCategorias) => this.categorias = listaCategorias
     });
   }
 
